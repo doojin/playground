2e2677ea2f72564a9ffe4988ceb349cab93bcb0a
doojin <dmitry.papka@gmail.com>
2019-05-29 10:30:48 -0300

    example of fetching data with timeout

--- await-timeout/src/index.js
+++ await-timeout/src/index.js
@@ -5,3 +5,16 @@ document.querySelector('#example1Button').addEventListener('click', async () => 
   await Timeout.set(timeout);
   alert('Hello, world!');
 });
+
+document.querySelector('#example2Button').addEventListener('click', async () => {
+  const url = document.querySelector('#example2urlInput').value;
+  const timeout = document.querySelector('#example2timeoutInput').value;
+
+  try {
+    const response = await await Timeout.wrap(fetch(url), timeout, 'Timeout exceeded');
+    const text = await response.text();
+    alert(text);
+  } catch (e) {
+    alert(`Fetch failed with error: ${e}`);
+  }
+});
