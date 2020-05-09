d106460e60428e37f7809b28445347d800bd6fac
doojin <dmitry.papka@gmail.com>
2020-05-08 19:40:50 -0500

    showing current video time

--- video-progress/app.js
+++ video-progress/app.js
@@ -1,11 +1,14 @@
 const progressElement = document.querySelector('progress');
 const videoElement = document.querySelector('video');
+const progressLabel = document.querySelector('#progressLabel');
 
 function updateProgress() {
     const totalVideoTime = videoElement.duration;
     const currentVideoTime = videoElement.currentTime;
     const progress = currentVideoTime / totalVideoTime;
+
     progressElement.value = progress;
+    progressLabel.textContent = `${currentVideoTime}s.`;
 }
 
 videoElement.oncanplay = () => {
