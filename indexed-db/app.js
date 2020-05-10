6fbb03b970ae6525cbe203d054cc18a300339c58
doojin <dmitry.papka@gmail.com>
2020-05-10 10:44:32 -0500

    displaying DB-related events in DOM

--- indexed-db/app.js
+++ indexed-db/app.js
@@ -2,21 +2,28 @@ const openRequest = indexedDB.open('store', 1);
 
 openRequest.addEventListener('upgradeneeded', () => {
     const db = openRequest.result;
-    console.log(`upgrade needed, db version: ${db.version}`);
+    logEvent(`Upgrade needed, db version: ${db.version}`);
 });
 
 openRequest.addEventListener('error', () => {
-    console.error(openRequest.error);
+    logEvent(`Error: ${openRequest.error}`);
 });
 
 openRequest.addEventListener('success', () => {
     const db = openRequest.result;
+    logEvent('Connection successful');
 
     // May happen when two tabs opened.
     // In second tab new version of db.
     // In first tab an outdated one.
     db.addEventListener('versionchange', () => {
         db.close();
-        console.log('page reload is needed');
+        logEvent('Version change; page reload is needed');
     });
 });
+
+function logEvent(event) {
+    const li = document.createElement('li');
+    li.textContent = event;
+    document.querySelector('#events').appendChild(li);
+}
