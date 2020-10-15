dc9a0df4168baaebd8f416342d44da16ac7827aa
doojin <dmitry.papka@gmail.com>
2020-10-15 10:28:17 -0500

    adding share button to each article if share API is supported by current browser

--- share-api/app.js
+++ share-api/app.js
@@ -1,3 +1,24 @@
-if (navigator.share) {
-  console.log('share API is supported by your browser!');
+function makeShareable(articleElement) {
+  if (navigator.share) {
+    function share(articleElement) {
+      navigator.share({
+        title: articleElement.querySelector('h1').innerText,
+        text: articleElement.querySelector('p').innerText,
+        url: window.location.href
+      });
+    }
+
+    function createShareButton() {
+      const shareButton = document.createElement('button');
+      shareButton.insertAdjacentText('afterbegin', 'Share');
+      shareButton.classList.add('share-button');
+      return shareButton;
+    }
+
+    const shareButton = createShareButton();
+    shareButton.addEventListener('click', () => share(articleElement));
+    articleElement.insertAdjacentElement('afterbegin', shareButton);
+  }
 }
+
+[...document.querySelectorAll('article')].forEach(makeShareable);
